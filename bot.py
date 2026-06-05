62963c008a3ab5d255c92985bb60d6e8118f35b4
fireHire-ai322 <firehirerecruiter@gmail.com>
2026-06-05 21:09:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,6 +30,7 @@ TEAM_MAP = {
     "I": {"tl_role": "TL-I", "rec_role": "Rec-I", "channel": "mariamh-team-i"},
     "J": {"tl_role": "TL-J", "rec_role": "Rec-J", "channel": "sayed-team-j"},
     "N": {"tl_role": "TL-N", "rec_role": "Rec-N", "channel": "aya-team-n"},
+    "E": {"tl_role": "TL-E", "rec_role": "Rec-E", "channel": "merna-team-e"},
 }
 
 intents = discord.Intents.default()
